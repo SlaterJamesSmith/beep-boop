451d82b6f17732a7f6d4f9357293a6d51fc25095
Slater Smith <smithslater@gmail.com>
2019-01-25 14:15:13 -0800

    create error message

--- js/scripts.js
+++ js/scripts.js
@@ -5,36 +5,44 @@ $(document).ready(function() {
 
 var userInput = parseInt($("input#number").val());
 
-var newInputs = []
-  for (i = 0; i < userInput + 1; i +=1) {
-    newInputs.push(userInput - (userInput - i))
-  }
-var rmvThrees = []
-  newInputs.forEach(function(newInput) {
-    if (String(newInput).includes("3")) {
-      rmvThrees.push("I'm sorry Dave. I'm afrain I can't do that.")
-    } else {
-      rmvThrees.push(newInput)
+  if (userInput >= 1) {
+
+  var newInputs = []
+    for (i = 0; i < userInput + 1; i +=1) {
+      newInputs.push(userInput - (userInput - i))
     }
-  });
-  var rmvTwos = []
-    rmvThrees.forEach(function(rmvThree) {
-      if (String(rmvThree).includes("2")) {
-        rmvTwos.push("Boop")
+  var rmvThrees = []
+    newInputs.forEach(function(newInput) {
+      if (String(newInput).includes("3")) {
+        rmvThrees.push("I'm sorry Dave. I'm afraid I can't do that.")
       } else {
-        rmvTwos.push(rmvThree)
+        rmvThrees.push(newInput)
       }
     });
-    var rmvOnes = []
-      rmvTwos.forEach(function(rmvTwo) {
-        if (String(rmvTwo).includes("1")) {
-          rmvOnes.push("Beep!")
+    var rmvTwos = []
+      rmvThrees.forEach(function(rmvThree) {
+        if (String(rmvThree).includes("2")) {
+          rmvTwos.push("Boop")
         } else {
-          rmvOnes.push(rmvTwo)
+          rmvTwos.push(rmvThree)
         }
       });
+      var rmvOnes = []
+        rmvTwos.forEach(function(rmvTwo) {
+          if (String(rmvTwo).includes("1")) {
+            rmvOnes.push("Beep!")
+          } else {
+            rmvOnes.push(rmvTwo)
+          }
+        });
 
-      $("#result").text(rmvOnes.join(", "))
+        $("#result").text(rmvOnes.join(", "))
+        $("#error").hide();
+        $("#result").show();
+    } else {
+      $("#error").show();
+      $("#result").hide();
+    }
 
 console.log(rmvOnes)
 });
